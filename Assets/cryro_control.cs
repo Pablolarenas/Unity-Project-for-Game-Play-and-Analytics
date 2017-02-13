d0a5f0740d83566aeaf88b5334aab0f2dd849828
Pablolarenas <pabloalarenas@gmail.com>
2017-02-13 22:53:04 +0000

    Inicial Commit - Added first functional mock-up of Borracho's Game

---
+++ Assets/cryro_control.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cryro_control : MonoBehaviour {
+
+    Rigidbody2D Borracho;
+    public float dificulty;
+    public float speed;
+    public Transform camera;
+
+
+    void Start()
+    {
+#if UNITY_ANDROID
+            Input.gyro.enabled = true;
+#endif
+
+        Borracho = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+       //Borracho's walk function
+        Borracho.AddForce(new Vector2(dificulty * Input.gyro.rotationRate.y, speed));
+
+        camera.localPosition = new Vector3(0,transform.localPosition.y+2.5f,-20);
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "" + transform.localPosition.y);
+        GUI.Label(new Rect(10, 30, 100, 20), "" + Input.gyro.rotationRate.x);
+    }
+}
